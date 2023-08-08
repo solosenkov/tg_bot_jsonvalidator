f7a67465552cec01b9912457abde0696a9a693e9
Evgenii <solosenkov@gmail.com>
2023-08-08 14:14:35 +0300

    Update jsn.py
    just check

--- jsn.py
+++ jsn.py
@@ -2,18 +2,18 @@ import telebot
 import json
 from json.decoder import JSONDecodeError
 
-# Сам бот
+
 bot = telebot.TeleBot("Insert token HERE")
 
-# Редактирование команды /start
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.reply_to(message, "Привет, ничтожество! Я бот, который может валидировать джсон. Вставляй его в сообщении и жми энтер")
 
-# Обработка ввода
+
 @bot.message_handler(content_types=['text'])
 def text_message(message):
-    # Проверяем, содержит ли сообщение валидный JSON
+    
     try:
         json_message = json.loads(message.text)
         bot.reply_to(message, "JSON - валидный.")
@@ -22,5 +22,5 @@ def text_message(message):
     except Exception as e:
         bot.reply_to(message, "Произошла ошибка при обработке сообщения: {}".format(e))
 
-# Запуск бота
+
 bot.polling()
